168c444eace63fb4b3fb8e998441d25ad9440d81
simonaspuceika <simonaspuceika@gmail.com>
2026-01-20 10:04:58 +0000

    14 /objekte I

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -40,13 +40,51 @@
 /* 02b. For-Schleife für Array-Index (Iteration)*/
 let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
 
-for (let i = 0; i < arr.length; i++) {
-    // output(i); // interner index
-    output(arr[i]); // Mapping intern --> extern
+// for (let i = 0; i < arr.length; i++) {
+//     // output(i); // interner index
+//     output(arr[i]); // Mapping intern --> extern
+// }
+
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+// //    a = a + 1
+//     a += 1;  
+//     output("inhalt von a: " + a);
+// }
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+ const GAP = " ";
+let str = ""; // Anfangswert
+for (let i = 0; i < 10; i++) {
+   str += output(arr[i]); 
+     GAP ; // a = a + 1
+  // output(str);
 }
-
-
-
 
 
 
---
+++ src/14_objects.js
@@ -0,0 +1,71 @@
+/***** Erinnerung: Arrays *******/
+
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
+
+// nested arrays - Prinzip: [[[]]]
+// nested arrays  - hier: [[],[]]
+// arr =   [
+//             ["Ich","bin","Max"],
+//             ["Ich","bin","Moritz"]
+//         ];
+      
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
+
+// // nested loop
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         output("Index i: " + i + " Index j: " + j);
+//         output(arr[i][j]);       
+//     }
+// }
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+let person = {
+                firstName: "Yasin",
+                lastName: "Warsame",
+                salary: [120000, 160000],
+                permission: true,
+                sayHello: function(){return "Hallo"},
+                sayHello2:function () {
+                    return "Hallo, ich bin " + this.firstName;
+                }
+
+};
+
+
+// output(person);
+output("---------------");
+output(person.firstName);
+output(person.lastName);
+output(person["lastName"])
+output("---------------");
+output(person.salary[1]);
+for (let i = 0; i < person.salary.length; i++) {
+    output(person.salary[i]);
+}
+output("---------------");
+output(person.permission);
+
+output("---------------")
+const txt = person.sayHello() + ", ich bin " + person.firstName +
+            " und verdiene " + person.salary[1] + " p.a.";
+output(txt)
+const txt2 = person.sayHello2() + " und verdiene " + person.salary[1] + " p.a.";
+output(txt2);
+
+
+// Modul: Ausgabe | Test
+//output("hi");
+function output(outputData) {
+    console.log(outputData);
+}
